9cfb1c49890aa3518bbbb1f14709559cc07735e5
Ha Hoang Hao <hhhao.vn@gmail.com>
2026-02-04 21:18:10 +0700

    feat: Add question strategies (#4)

---
+++ src/surpy/questions/strategies/__init__.py
@@ -0,0 +1,18 @@
+from .single_strategy import SingleStrategy
+from .multiple_strategy import MultipleStrategy
+from .matrix_single_strategy import MatrixSingleStrategy
+from .matrix_multiple_strategy import MatrixMultipleStrategy
+from .number_strategy import NumberStrategy
+from .rank_strategy import RankStrategy
+from .text_strategy import TextStrategy
+
+
+__all__ = [
+    "SingleStrategy",
+    "MultipleStrategy",
+    "MatrixSingleStrategy",
+    "MatrixMultipleStrategy",
+    "NumberStrategy",
+    "RankStrategy",
+    "TextStrategy",
+]
---
+++ src/surpy/questions/strategies/text_strategy.py
@@ -0,0 +1,25 @@
+import polars as pl
+
+from .strategy import QuestionStrategy
+from ..option import Option
+
+
+class TextStrategy(QuestionStrategy):
+    def __init__(
+        self,
+        **kwargs,
+    ) -> None:
+        self.id: str = kwargs["id"]
+        self.text: str = kwargs["text"]
+        self.options: list[Option] = kwargs["options"]
+        self.data: dict = kwargs["data"]
+        self.response_ids: dict = kwargs["response_ids"]
+
+    def _validate_data(self, data: dict) -> None: ...
+
+    @property
+    def dtype(self) -> str: ...
+
+    def get_df(self) -> pl.DataFrame: ...
+
+    def describe(self) -> pl.DataFrame: ...
