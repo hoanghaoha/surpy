e61d85c436e0cc7e2e2c5e2b20b9921d81ab5a01
hhhao <hhhao.vn@gmail.com>
2026-02-05 12:36:21 +0700

    wip: add single_strategy functions

--- src/surpy/questions/strategies/single_strategy.py
+++ src/surpy/questions/strategies/single_strategy.py
@@ -1,8 +1,10 @@
+from typing import Literal
 import polars as pl
 
 from .strategy import QuestionStrategy
 from ..option import Option
 from ...errors import DataError
+from ...config import Identifier
 
 
 class SingleStrategy(QuestionStrategy):
@@ -22,20 +24,62 @@ class SingleStrategy(QuestionStrategy):
                 f"Length of Single question data can not be larger than 1: {self.id} is {len(data)}"
             )
 
-        if len(data) != len(self.response_ids):
+        if len(data[1]) != len(self.response_ids):
             raise DataError("Lenght of data is different with length of response_ids")
 
         if 1 not in data.keys():
             raise DataError("Single question data must have 1 in keys")
 
-        self.data = data[1]
+        if not all([isinstance(d, (str, int)) for d in self.data]):
+            raise DataError("Single question data must be int or str")
+
+        self.data: list[str | int | None] = data[1]
 
     @property
-    def dtype(self) -> str:
-        if any([isinstance(d, (int, float)) for d in self.data]):
+    def dtype(self) -> Literal["number", "text"]:
+        if all([isinstance(d, int) or d is None for d in self.data]):
             return "number"
         return "text"
 
-    def get_df(self) -> pl.DataFrame: ...
+    @property
+    def _t2n(self) -> dict[str, int]:
+        return {op.text: op.index for op in self.options}
 
-    def describe(self) -> pl.DataFrame: ...
+    @property
+    def _n2t(self) -> dict[int, str]:
+        return {op.index: op.text for op in self.options}
+
+    def change_dtype(self, to: Literal["number", "text"]):
+        if self.dtype == "text" and to == "number":
+            str_data: list[str] = [str(d) for d in self.data]
+            self.data = [{**self._t2n, **{"None": None}}.get(d, None) for d in str_data]
+        if self.dtype == "number" and to == "text":
+            number_data: list[int] = [int(d) if d is not None else 0 for d in self.data]
+            self.data = [{**self._n2t, **{0: None}}.get(d, None) for d in number_data]
+
+    def get_df(self) -> pl.DataFrame:
+        return pl.DataFrame({self.id: self.data, Identifier.Id: self.response_ids})
+
+    def describe(self) -> pl.DataFrame:
+        current_dtype = self.dtype
+
+        if current_dtype == "number":
+            self.change_dtype("text")
+
+        df = self.get_df()
+
+        self.change_dtype(current_dtype)
+
+        describe_df = (
+            df.group_by(self.id)
+            .agg(
+                pl.n_unique(Identifier.Id).alias("count"),
+            )
+            .sort(pl.col(self.id).replace(self._t2n))
+            .with_columns(
+                (pl.col("count") / pl.sum("count")).alias("percent"),
+                (pl.col("count") / pl.sum("count")).cum_sum().alias("cum_percent"),
+            )
+        )
+
+        return describe_df
